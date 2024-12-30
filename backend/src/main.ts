81e1a6422f21b2725ac22236a06955619b94fe08
Olle Thunberg <olle.thunberg03@gmail.com>
2024-12-30 15:30:17 +0100

    feat: room setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,11 +8,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/', (_req, res) => {
+app.get('/', (req, res) => {
         res.send({ uptime: process.uptime(), });
 });
 
-const server = http.createServer();
+app.get('/rooms', (_req, res) => {
+        res.send({ rooms });
+})
+
+
+const server = http.createServer(app);
 const io = new socketio.Server(server, {
         cors: {
                 origin: '*',
@@ -20,16 +25,43 @@ const io = new socketio.Server(server, {
         },
 });
 
-io.on('connection', (socket) => {
-        socket.on('disconnect', async () => {
-                console.log('User disconnected:');
+type Player = {
+        name: string;
+        socketId: string;
+        dice: [number, number, number, number, number]
+        //health: number
+}
 
+const rooms: { [key: string]: { players: Player[] } } = {}
+
+
+
+io.on('connection', (socket) => {
+        console.log('User connected');
+
+        socket.on('disconnect', async () => {
+                console.log('User disconnected');
 
         });
 
+        socket.on('setName', (name) => {
+                console.log('Name:', name);
+        })
+        socket.on('joinRoom', (room) => {
+                if (!rooms[room]) {
+                        rooms[room] = { players: [] }
+                }
+
+                rooms[room].players.push({
+                        name: 'Player',
+                        socketId: socket.id,
+                        dice: [0, 0, 0, 0, 0]
+                })
+
+                socket.join(room);
+        })
 });
 
 server.listen(4000, () => {
         console.log('Server listening on port 4000');
 });
-
