4a62ed22b76fd5c887a5131befc701ec3211e1dd
HellgrenR <rallenolltva@gmail.com>
2024-12-31 16:43:24 +0100

    fix: add userName to lobby state

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       return;
     }
     socket.emit('joinRoom', roomName);
-    navigate('/lobby', { state: { roomName } });
+    navigate('/lobby', { state: { roomName, userName } });
   }
 
   function getRooms() {
