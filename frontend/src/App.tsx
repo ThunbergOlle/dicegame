79112ca720583f6db54dd89289b67c62b360df91
HellgrenR <rallenolltva@gmail.com>
2024-12-31 16:16:45 +0100

    feat: add game

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/home"
 import Lobby from "./pages/lobby"
+import Game from "./pages/game"
 
 export default function App() {
 
@@ -8,6 +9,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/lobby" element={<Lobby />} />
+      <Route path="/lobby/game" element={<Game />} />
     </Routes>
 
   )
--- frontend/src/pages/lobby.tsx
+++ frontend/src/pages/lobby.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { socket } from "../socket";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Lobby() {
   const location = useLocation();
+  const navigate = useNavigate();
   const roomName = location.state.roomName;
   const [players, setPlayers] = useState([]);
 
@@ -19,12 +20,13 @@ export default function Lobby() {
 
   function startGame() {
     socket.emit('');
+    navigate('/lobby/game');
   }
 
   return (
     <div>
       <h1>{roomName}</h1>
-      <button onClick={() => startGame()}>Start game</button>
+      <button onClick={() => startGame()}>Start Game</button>
       <h2>Players:</h2>
       {players.map((player) => (
         <div key={player}>{player["name"]}</div>
