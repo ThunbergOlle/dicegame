8648c8cd25cc559a8aebb696e2005ad578c4ddd9
Olle Thunberg <olle.thunberg03@gmail.com>
2024-12-31 14:06:31 +0100

    fix: correct formatting

--- backend/src/game.ts
+++ backend/src/game.ts
@@ -1,96 +1,99 @@
-import { Server as SocketIOServer, Socket } from "socket.io";
+import { Server as SocketIOServer, Socket } from 'socket.io';
 
 const rollDice = () => Math.floor(Math.random() * 6) + 1;
 
 export class Player {
-        name: string;
-        socket: Socket;
-        dice: [number, number, number, number, number]
+  name: string;
+  socket: Socket;
+  dice: [number, number, number, number, number];
 
-        constructor(name: string, socket: Socket) {
-                this.name = name
-                this.dice = [1, 1, 1, 1, 1]
-                this.socket = socket
-        }
+  constructor(name: string, socket: Socket) {
+    this.name = name;
+    this.dice = [1, 1, 1, 1, 1];
+    this.socket = socket;
+  }
 
+  roll() {
+    this.dice = [rollDice(), rollDice(), rollDice(), rollDice(), rollDice()];
 
-        roll() {
-                this.dice = [rollDice(), rollDice(), rollDice(), rollDice(), rollDice()]
-
-                this.socket.emit('dice', this.dice)
-        }
-
-
+    this.socket.emit('dice', this.dice);
+  }
 }
 type GameRound = {
-        currentPlayer: string;
-        diceValue: number;
-        diceCount: number;
-}
+  currentPlayer: string;
+  diceValue: number;
+  diceCount: number;
+};
 
 export class Game {
-        public players: Array<Player>
-        private server: SocketIOServer
-        private roomName: string
+  public players: Array<Player>;
+  private server: SocketIOServer;
+  private roomName: string;
 
-        public round: GameRound
-        started: boolean = false
+  public round: GameRound;
+  started: boolean = false;
 
-        constructor(server: SocketIOServer, roomName: string) {
-                this.players = []
-                this.round = {
-                        currentPlayer: '',
-                        diceValue: 0,
-                        diceCount: 0
-                }
-                this.server = server
-                this.roomName = roomName
-        }
+  constructor(server: SocketIOServer, roomName: string) {
+    this.players = [];
+    this.round = {
+      currentPlayer: '',
+      diceValue: 0,
+      diceCount: 0,
+    };
+    this.server = server;
+    this.roomName = roomName;
+  }
 
-        addPlayer(player: Player) {
-                this.players.push(player)
+  addPlayer(player: Player) {
+    this.players.push(player);
 
-                this.server.to(this.roomName).emit('players', this.players.map(player => player.name))
-        }
+    this.server.to(this.roomName).emit(
+      'players',
+      this.players.map((player) => player.name),
+    );
+  }
 
-        removePlayer(socketId: string) {
-                this.players = this.players.filter(player => player.socket.id !== socketId)
+  removePlayer(socketId: string) {
+    this.players = this.players.filter((player) => player.socket.id !== socketId);
 
-                this.server.to(this.roomName).emit('players', this.players.map(player => player.name))
-        }
+    this.server.to(this.roomName).emit(
+      'players',
+      this.players.map((player) => player.name),
+    );
+  }
 
-        getPlayer(socketId: string) {
-                return this.players.find(player => player.socket.id === socketId)
-        }
+  getPlayer(socketId: string) {
+    return this.players.find((player) => player.socket.id === socketId);
+  }
 
-        nextRound() {
-                this.players.forEach(player => player.roll())
+  nextRound() {
+    this.players.forEach((player) => player.roll());
 
-                this.round.currentPlayer = this.players[0].name
-                this.round.diceValue = 0
-                this.round.diceCount = 0
+    this.round.currentPlayer = this.players[0].name;
+    this.round.diceValue = 0;
+    this.round.diceCount = 0;
 
-                this.server.to(this.roomName).emit('round', this.round)
-        }
+    this.server.to(this.roomName).emit('round', this.round);
+  }
 
-        nextPlayer() {
-                const currentPlayerIndex = this.players.findIndex(player => player.name === this.round.currentPlayer)
+  nextPlayer() {
+    const currentPlayerIndex = this.players.findIndex((player) => player.name === this.round.currentPlayer);
 
-                if (currentPlayerIndex === this.players.length - 1) {
-                        this.round.currentPlayer = this.players[0].name
-                } else {
-                        this.round.currentPlayer = this.players[currentPlayerIndex + 1].name
-                }
+    if (currentPlayerIndex === this.players.length - 1) {
+      this.round.currentPlayer = this.players[0].name;
+    } else {
+      this.round.currentPlayer = this.players[currentPlayerIndex + 1].name;
+    }
 
-                this.server.to(this.roomName).emit('round', this.round)
-        }
+    this.server.to(this.roomName).emit('round', this.round);
+  }
 
-        start() {
-                this.started = true
-                this.server.to(this.roomName).emit('startGame')
+  start() {
+    this.started = true;
+    this.server.to(this.roomName).emit('startGame');
 
-                sleep(1000).then(this.nextRound)
-        }
+    sleep(1000).then(this.nextRound);
+  }
 }
 
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,80 +10,74 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/', (req, res) => {
-        res.send({ uptime: process.uptime(), });
+  res.send({ uptime: process.uptime() });
 });
 
 app.get('/rooms', (_req, res) => {
-        res.send({ rooms });
-})
+  res.send({ rooms });
+});
 
 app.get('/room/:id', (req, res) => {
-        const room = req.params.id;
-        res.send({ players: rooms[room].players });
-})
-
+  const room = req.params.id;
+  res.send({ players: rooms[room].players });
+});
 
 const server = http.createServer(app);
 const io = new socketio.Server(server, {
-        cors: {
-                origin: '*',
-                methods: ['GET', 'POST'],
-        },
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
 });
 
-
 const rooms: {
-        [key: string]: Game
-} = {}
-
-
+  [key: string]: Game;
+} = {};
 
 io.on('connection', (socket: socketio.Socket) => {
-        let room: string;
-        console.log('User connected');
+  let room: string;
+  console.log('User connected');
 
+  socket.on('getRooms', () => {
+    socket.emit('rooms', Object.keys(rooms));
+  });
 
-        socket.on('getRooms', () => {
-                socket.emit('rooms', Object.keys(rooms));
-        });
+  socket.on('disconnect', async () => {
+    console.log('User disconnected');
+  });
 
-        socket.on('disconnect', async () => {
-                console.log('User disconnected');
+  socket.on('setName', (name: string) => {
+    const player = rooms[room].getPlayer(socket.id);
+    if (!player) {
+      throw new Error('Player not found');
+    }
 
-        });
+    player.name = name;
+  });
 
-        socket.on('setName', (name: string) => {
-                const player = rooms[room].getPlayer(socket.id);
-                if (!player) {
-                        throw new Error('Player not found');
-                }
+  socket.on('joinRoom', (joinRoom: string) => {
+    room = joinRoom;
+    socket.join(joinRoom);
 
-                player.name = name;
-        })
+    if (!rooms[joinRoom]) {
+      rooms[joinRoom] = new Game(io, joinRoom);
+    }
 
-        socket.on('joinRoom', (joinRoom: string) => {
-                room = joinRoom;
-                socket.join(joinRoom);
+    if (rooms[joinRoom].started) {
+      throw new Error('Game already started');
+    }
 
-                if (!rooms[joinRoom]) {
-                        rooms[joinRoom] = new Game(io, joinRoom);
-                }
+    rooms[joinRoom].addPlayer(new Player('Player', socket));
+  });
 
-                if (rooms[joinRoom].started) {
-                        throw new Error('Game already started');
-                }
-
-                rooms[joinRoom].addPlayer(new Player('Player', socket));
-        })
-
-        socket.on('startGame', () => {
-                if (!room) {
-                        throw new Error('Room not set');
-                }
-                rooms[room].start()
-        })
+  socket.on('startGame', () => {
+    if (!room) {
+      throw new Error('Room not set');
+    }
+    rooms[room].start();
+  });
 });
 
 server.listen(4000, () => {
-        console.log('Server listening on port 4000');
+  console.log('Server listening on port 4000');
 });
