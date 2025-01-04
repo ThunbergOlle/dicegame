3a7b77ca7e00fba727909cb68de6a5a89b625b91
HellgrenR <rallenolltva@gmail.com>
2025-01-04 16:00:46 +0100

    prepare Game page

--- frontend/src/pages/game.tsx
+++ frontend/src/pages/game.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+
 export default function Game() {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    
+
+    return () => {
+    }
+  }, []);
 
   return (
+    // have a list of other players at the top, spanning horizontally
+    // show the current bet in the middle of the screen
+    // have the current player's dice in the bottom middle
     <>
+      <div>{}</div>
     </>
   )
 }
